b6309f981f2b0d974f0eb5fe7a1b215010ee35a3
Dave ZAGABE <80624123+DaveZag@users.noreply.github.com>
2022-10-05 15:27:55 +0200

    Create a simple calculator and write tests for all its operations

--- index.js
+++ index.js
@@ -1,4 +1,42 @@
+// function to returning the length of a string
 function stringLength(string) {
-  return string.length;
+  if (string.length >= 1 && string.length <= 10) {
+    return string.length;
+  }
+  throw new Error("The string's length should be between 1-10 characters");
 }
-module.exports = stringLength;
+
+// function to reverse a string
+function reverseString(string) {
+  return string.split("").reverse().join("");
+}
+
+// simple calculator
+class Calculator {
+  constructor(firstNumber, secondNumber) {
+    this.firstNumber = firstNumber;
+    this.secondNumber = secondNumber;
+  }
+
+  add() {
+    return this.firstNumber + this.secondNumber;
+  }
+
+  substract() {
+    return this.firstNumber - this.secondNumber;
+  }
+
+  multiply() {
+    return this.firstNumber * this.secondNumber;
+  }
+
+  divide() {
+    return this.firstNumber / this.secondNumber;
+  }
+}
+
+const calc = new Calculator(9, 1);
+
+exports.stringLength = stringLength;
+exports.reverseString = reverseString;
+exports.Calculator = Calculator;
--- index.test.js
+++ index.test.js
@@ -1,4 +1,81 @@
-const stringLength = require("./index.js");
+const { stringLength, reverseString, Calculator } = require("./index.js");
+
+// test for the string length function
 test("Properly returns the number of characters in a string", () => {
-  expect(stringLength("hello")).toBe(string.length);
+  const greet = "Hello";
+  expect(stringLength(greet)).toBe(5);
+});
+
+// test for the reverse string function
+test("Properly reverse a string", () => {
+  const greet = "hello";
+  expect(reverseString(greet)).toBe("olleh");
+});
+
+// test for addition operation
+describe("calculator add operation", () => {
+  const calculate = new Calculator(5, 98);
+
+  test("Properly adds two numbers", () => {
+    expect(calculate.add()).toBe(103);
+  });
+
+  test("Is greater than zero", () => {
+    expect(calculate.add()).toBeGreaterThan(0);
+  });
+
+  test("Is not null", () => {
+    expect(calculate.add()).not.toBeNull();
+  });
+});
+
+// Test for substraction operation
+describe("calculator substract operation", () => {
+  const calculate = new Calculator(45, 60);
+
+  test("Is smaller than zero", () => {
+    expect(calculate.substract()).toBeLessThan(0);
+  });
+
+  test("Properly substracts two numbers", () => {
+    expect(calculate.substract()).toBe(-15);
+  });
+
+  test("Is not null", () => {
+    expect(calculate.substract()).not.toBeNull();
+  });
+});
+
+// tests for dividion operation
+describe("calculator divide operations ", () => {
+  const calculate = new Calculator(20, 4);
+
+  test("Properly divides two numbers", () => {
+    expect(calculate.divide()).toBe(5);
+  });
+
+  test("Returns a defined value", () => {
+    expect(calculate.divide()).toBeDefined();
+  });
+
+  test("Is not false", () => {
+    expect(calculate.divide()).not.toBeFalsy();
+  });
+});
+
+// tests for multiplication operation
+describe("calculator divide operations ", () => {
+  const calculate = new Calculator(2, 4);
+
+  test("Properly multiplies two numbers", () => {
+    expect(calculate.multiply()).toBe(8);
+  });
+
+  test("Returns a defined value", () => {
+    expect(calculate.multiply()).toBeDefined();
+  });
+
+  test("Is true", () => {
+    expect(calculate.multiply()).toBeTruthy();
+  });
 });
